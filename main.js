3438dfceb4cccb0ed34bad7efb46ff51bc317080
Vedant207 <83002168+Vedant207@users.noreply.github.com>
2021-08-17 11:33:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ ctx.arc(250, 210, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "yellow";
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
 ctx.lineWidth = 5;
 ctx.arc(300, 250, 40, 0, 2 * Math.PI);
 ctx.stroke();
